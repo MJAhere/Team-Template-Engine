17e430007f88936f2b7f20dd53f93d452da842a4
mjahere023@gmail.com <mjahere023@gmail.com>
2020-02-19 06:56:09 -0700

    Worked out readMe, and finished up functionality

--- MyREADME.md
+++ MyREADME.md
@@ -1,1 +1,13 @@
 # Team-Template-Engine
+
+## Download
+- Npm i and npm i jest
+
+## Instructions
+- Enter node app,js in your command line to prompt the team gen questions.
+- Once finished answering you should see a "My Team is Complete" message. 
+- You can find your file in the output folder. It will be called team.html
+
+## Photo of team.html file when set-up in browser.
+![Team Template 2020-02-19 061502](https://user-images.githubusercontent.com/57784815/74840712-b02d4780-52e4-11ea-9a68-06a1e9abf67c.png)
+
--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Keep these lines; they're important!
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -12,24 +11,145 @@ const render = require("./lib/htmlRenderer");
 
 const team = [];
 
-class teamBuilder {
+class teamViewer {
     confirmManager() {
         inquirer
             .prompt([
                 {
                     type: "confirm"
-                    , message: "Are you the team manager?"
+                    , message: "Are you the manager?"
                     , name: "managerConfirm"
                 }
             ]).then(res => {
                 if (res.managerConfirm) {
                     this.createManager()
                 } else {
-                    console.log("You need to be a manager to create a team!")
+                    console.log("Sorry, only managers can construct a team")
                     process.exit(0);
                 }
             })
     };
+
+    createManager() {
+        inquirer
+            .prompt([
+                {
+                    type: "input"
+                    , message: "Please enter your name"
+                    , name: "managerName"
+                },
+                {
+                    type: "input"
+                    , message: "Please enter your manager ID number"
+                    , name: "managerIDNum"
+                },
+                {
+                    type: "input"
+                    , message: "Please enter your office phone number"
+                    , name: "managerPhone"
+                },
+                {
+                    type: "input"
+                    , message: "Please enter your email address"
+                    , name: "managerEmail"
+                },
+            ]).then(res => {
+                const manager = new Manager(res.managerName, res.managerIDNum, res.managerEmail, res.managerPhone);
+                team.push(manager);
+                this.createTeam();
+            });
+    };
+
+    createTeam() {
+        inquirer
+            .prompt([
+                {
+                    type: "list"
+                    , message: "Would you like more team members, or are you finished?"
+                    , choices: ["Engineer", "Intern", "My team is complete"]
+                    , name: "employeeType"
+                }
+            ]).then(res => {
+                if (res.employeeType === "Engineer") {
+                    this.createEngineer();
+                } else if (res.employeeType === "Intern") {
+                    this.createIntern();
+                } else {
+                    fs.writeFile(outputPath, render(team), err => {
+                        if (err) throw err;
+                        console.log("EUREKA!")
+                    })
+
+                }
+            });
+    };
+
+    createEngineer() {
+        inquirer
+            .prompt([
+                {
+                    type: "input"
+                    , message: "Please type the engineer's name"
+                    , name: "engineerName"
+                },
+                {
+                    type: "input"
+                    , message: "Please enter the engineer's ID number"
+                    , name: "engineerIDNum"
+                },
+                {
+                    type: "input"
+                    , message: "Please enter the engineer's GitHub"
+                    , name: "engineerPhone"
+                },
+                {
+                    type: "input"
+                    , message: "Please enter engineer's email"
+                    , name: "engineerEmail"
+                },
+            ]).then(res => {
+                const engineer = new Engineer(res.engineerName, res.engineerIDNum, res.engineerEmail, res.engineerPhone);
+                team.push(engineer);
+                this.createTeam();
+            });
+    };
+
+    createIntern() {
+        inquirer
+            .prompt([
+                {
+                    type: "input"
+                    , message: "Please enter the intern's name"
+                    , name: "internName"
+                },
+                {
+                    type: "input"
+                    , message: "Please enter the intern's ID number"
+                    , name: "internIDNum"
+                },
+                {
+                    type: "input"
+                    , message: "Please enter the intern's school name"
+                    , name: "internSchool"
+                },
+                {
+                    type: "input"
+                    , message: "Please enter the intern's email address"
+                    , name: "internEmail"
+                },
+            ]).then(res => {
+                const intern = new Intern(res.internName, res.internIDNum, res.internEmail, res.internSchool);
+                team.push(intern);
+                this.createTeam();
+            });
+    }
+
+
+}
+
+const viewTeam = new teamViewer();
+
+viewTeam.confirmManager();
 
 
 // Write code to use inquirer to gather information about the development team members,
